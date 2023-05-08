23b71ec6c0b16fca1302f036fea1f8a429ca48e1
Yurii Zahrai <66515294+yuriizahrai@users.noreply.github.com>
2023-05-08 20:53:47 +0300

    fix

---
+++ config/default.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.default = {
+    port: 3000,
+    dbURL: 'mongodb+srv://Frodo:kjhlcfehjy@cluster0.dqg2t1i.mongodb.net/'
+};
+//# sourceMappingURL=default.js.map
--- index.js
+++ index.js
@@ -1,1 +1,3 @@
+"use strict";
 console.log("hellow world");
+//# sourceMappingURL=index.js.map
