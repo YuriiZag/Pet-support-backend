5736390d1906d443578c6cbb154f9167a5029caa
Yurii Zahrai <66515294+yuriizahrai@users.noreply.github.com>
2023-05-09 18:55:32 +0300

    test

--- src/routes.ts
+++ src/routes.ts
@@ -3,7 +3,7 @@ import asyncWrapper from "./heplers/asyncWrapper"
 import { addPetCTRL } from "./controllers/petController"
 
 const routes = (app: Express) => {
-    app.get('/lol', (req: Request, res: Response)=> res.sendStatus(200))
+    app.get('/lol', (req: Request, res: Response) => res.sendStatus(200).json('orest loh'))
     app.post('/api/pets', asyncWrapper(addPetCTRL))
 }
 export default routes
