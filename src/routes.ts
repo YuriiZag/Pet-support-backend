683448186c6bb40bcf4807ad48409d07c2185f00
Yurii Zahrai <66515294+yuriizahrai@users.noreply.github.com>
2023-05-10 21:00:35 +0300

    little fix

--- src/routes.ts
+++ src/routes.ts
@@ -1,13 +1,14 @@
 import { Express, Request, Response } from "express";
 import asyncWrapper from "./heplers/asyncWrapper";
 import { addPetCTRL, getAllPetsCTRL } from "./controllers/petController";
+import { getNoticesByTitleCTRL } from "./controllers/noticesController";
 
 const routes = (app: Express) => {
   app.get("/api/pets", asyncWrapper(getAllPetsCTRL));
   app.get("/lol", (req: Request, res: Response) => res.sendStatus(200).json());
   app.post("/api/pets", asyncWrapper(addPetCTRL));
 
-  app.get("/api/notices/:title",);
+  app.get("/api/notices/:title", asyncWrapper(getNoticesByTitleCTRL));
   app.get("/api/notices/:category");
   app.get("/api/notices/:id");
   app.get("/api/notices/favorite");
