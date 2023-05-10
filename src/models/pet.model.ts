af034c2359019e5d8debef345f7a23ba3c75a536
Orest Mazghal <109895411+Lordfedot@users.noreply.github.com>
2023-05-10 11:45:05 +0300

    add pet schema

--- src/models/pet.model.ts
+++ src/models/pet.model.ts
@@ -1,16 +1,27 @@
 import mongoose from "mongoose";
 
 const petSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: [true, "Set name for contact"],
-      }, 
+  name: {
+    type: String,
+    required: [true, "Set name for contact"],
+  },
+  animal: {
+    type: String,
+    require: true,
+  },
+  breed: {
+    type: String,
+    require: true,
+  },
+  place: {
+    type: String,
+    require: true,
+  },
+  age: {
+    type: Number,
+  },
+});
 
-    age: {
-        type: Number
-    }
-})
+const Pet = mongoose.model("Pet", petSchema);
 
-const Pet = mongoose.model('Pet', petSchema )
-
-export default Pet
+export default Pet;
--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,10 @@
 import { Express, Request, Response } from "express"
 import asyncWrapper from "./heplers/asyncWrapper"
-import { addPetCTRL } from "./controllers/petController"
+import { addPetCTRL, getAllPetsCTRL } from "./controllers/petController"
 
 const routes = (app: Express) => {
-    app.get('/lol', (req: Request, res: Response) => res.sendStatus(200).json('orest loh'))
+    app.get('/api/pets', asyncWrapper(getAllPetsCTRL))
+    app.get('/lol', (req: Request, res: Response) => res.sendStatus(200).json())
     app.post('/api/pets', asyncWrapper(addPetCTRL))
 }
 export default routes
