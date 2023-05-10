783a86c7e0d2897ec709316be4259ff34d980a85
Yurii Zahrai <66515294+yuriizahrai@users.noreply.github.com>
2023-05-10 20:58:39 +0300

    first part done

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,19 @@
-import { Express, Request, Response } from "express"
-import asyncWrapper from "./heplers/asyncWrapper"
-import { addPetCTRL, getAllPetsCTRL } from "./controllers/petController"
+import { Express, Request, Response } from "express";
+import asyncWrapper from "./heplers/asyncWrapper";
+import { addPetCTRL, getAllPetsCTRL } from "./controllers/petController";
 
 const routes = (app: Express) => {
-    app.get('/api/pets', asyncWrapper(getAllPetsCTRL))
-    app.get('/lol', (req: Request, res: Response) => res.sendStatus(200).json())
-    app.post('/api/pets', asyncWrapper(addPetCTRL))
-}
-export default routes
+  app.get("/api/pets", asyncWrapper(getAllPetsCTRL));
+  app.get("/lol", (req: Request, res: Response) => res.sendStatus(200).json());
+  app.post("/api/pets", asyncWrapper(addPetCTRL));
+
+  app.get("/api/notices/:title",);
+  app.get("/api/notices/:category");
+  app.get("/api/notices/:id");
+  app.get("/api/notices/favorite");
+  app.get("/api/notices/privat");
+  app.get("/api/notices/privat/favourite");
+  app.post("/api/notices");
+  app.delete("/api/notices/:id");
+};
+export default routes;
