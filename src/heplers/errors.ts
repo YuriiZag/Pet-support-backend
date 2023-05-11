9d4d3ff74644528814122add20d8629f3a433dec
Orest Mazghal <109895411+Lordfedot@users.noreply.github.com>
2023-05-11 14:08:53 +0300

    add errors, custom error handler, pets controller

---
+++ src/heplers/errors.ts
@@ -0,0 +1,47 @@
+import { NextFunction, Request, Response } from "express";
+
+export class MyCustomError extends Error {
+  status = 400;
+  constructor(message: string) {
+    super(message);
+  }
+}
+export class ValidationError extends MyCustomError {
+  status = 400;
+  constructor(message: string) {
+    super(message);
+  }
+}
+
+export class ConflictError extends MyCustomError {
+  status = 409;
+  constructor(message: string) {
+    super(message);
+  }
+}
+
+export class WrongParametersError extends MyCustomError {
+  status = 404;
+  constructor(message: string) {
+    super(message);
+  }
+}
+
+export class NotAuthorizedError extends MyCustomError {
+  status = 401;
+  constructor(message: string) {
+    super(message);
+  }
+}
+
+export const errorHadler = (
+  err: Error,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  if (err instanceof MyCustomError) {
+    return res.status(err.status).json({ message: err.message });
+  }
+  return res.status(500).json({ message: err.message });
+};
