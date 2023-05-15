07101b5ee59904ea4807936fe06eca7a1d86c2fc
Orest Mazghal <109895411+Lordfedot@users.noreply.github.com>
2023-05-15 22:49:37 +0300

    add files

--- src/controllers/serviceController.ts
+++ src/controllers/serviceController.ts
@@ -1,20 +1,21 @@
 import { Request, Response, NextFunction } from "express";
-import { addService,getAllServices } from "../services/services";
+import { addService, getAllServices } from "../services/services";
 
 export const addServiceCTRL = async (
-    req: Request,
-    res: Response,
-    next: NextFunction
-  ) => {
-    const response = await addService(req.body);
-    return res.status(201).json({ message: "Service added", response });
-  };
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const path = req.file?.path as string
+  const response = await addService(req.body, path);
+  return res.status(201).json({ message: "Service added", response });
+};
 
-  export const getAllServicesCTRL = async (
-    req: Request,
-    res: Response,
-    next: NextFunction
-  ) => {
-    const response = await getAllServices();
-    return res.status(201).json({ message: "Success", response });
-  };
+export const getAllServicesCTRL = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const response = await getAllServices();
+  return res.status(201).json({ message: "Success", response });
+};
